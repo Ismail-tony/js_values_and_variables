2171d48c348725c0650d9ea9ec7a18970ee2bebf
Ismail-tony <ibabalola21@outlook.com>
2024-01-12 16:43:38 +0000

    added country, continent and population variable

---
+++ script.js
@@ -0,0 +1,9 @@
+var Country;
+var Continent;
+var Population;
+
+Country = 'Nigeria';
+Continent = 'Africa';
+Population = '226.2M'
+
+console.log('My name is Ismail Babalola. I am from ' + Country + ' a country in  ' + Continent + ". The population of "+ Country + ' is ' + Population + '.');
