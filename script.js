c5b6bf3abed1e99f17c3909d5010777d8eba1695
Ismail-tony <ibabalola21@outlook.com>
2024-01-13 12:45:45 +0000

    data types and basic operators

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-var Country;
-var Continent;
-var Population;
-
-Country = 'Nigeria';
-Continent = 'Africa';
-Population = '226.2M'
-
+//VALUES AND VARIABLES//
+const Country = 'Nigeria';
+const Continent = 'Africa';
+let Population;
+Population = 226.2
 console.log('My name is Ismail Babalola. I am from ' + Country + ' a country in  ' + Continent + ". The population of "+ Country + ' is ' + Population + '.');
+
+//DATA TYPES//
+const isIsland = false;
+const language = 'Yoruba';
+console.log(typeof isIsland);
+console.log(typeof Population);
+console.log(typeof Country);
+console.log(typeof language);
+
+//BASIC OPERATOR//
+console.log(Population/2);
+console.log(Population++);
+console.log(Population > 6);
+console.log(Population < 33);
+
+const description = Country + 
+' is in ' + 
+Continent + 
+' , and 1/3 of its ' + 
+Population + 
+' million people speak ' + 
+language;
+console.log(description);
